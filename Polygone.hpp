8771f1ed4bba39c8a692c4fef655af4585b10117
ASSE Romain <asse-romain@outlook.fr>
2024-01-16 09:53:37 +0100

    Ajout de la définition de la classe Polygone

--- Polygone.hpp
+++ Polygone.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <vector>
+#include <ostream>
 #include "Point2D.hpp"
+
+using namespace std;
 
 template <typename T>
 class Polygone
 {
     private:
-        Vector<Point2D<T>> sommets;
+        vector<Point2D<T>> sommets;
 
     public:
         // Constructeur par défaut
@@ -17,15 +20,84 @@ class Polygone
         Polygone(vector<Point2D<T>> listeSommets);
 
         // Constructeur par recopie
-        Polygone(Polygone<T> poly);
+        Polygone(const Polygone<T>& poly);
 
         // Accesseurs get
         vector<Point2D<T>> getSommets() const;
 
         // Accesseurs set
-        void setSommets(vector<Point2D<T>> listeSommets);
+        void setSommets(const vector<Point2D<T>>& listeSommets);
 
-        void addPoint(Point2D<T>);
+        // Permet d'ajouter un point
+        void addPoint(const Point2D<T>& point);
 
         void translate(T x, T y);
 };
+
+// Constructeur par défaut
+template <typename T>
+Polygone<T>::Polygone()
+{
+
+}
+
+// Constructeur avec des valeurs initiales
+template <typename T>
+Polygone<T>::Polygone(vector<Point2D<T>> listeSommets) : sommets(listeSommets)
+{
+
+}
+
+// Constructeur par recopie
+template <typename T>
+Polygone<T>::Polygone(const Polygone<T>& poly) : sommets(poly.sommets)
+{
+
+}
+
+// Accesseurs get
+template <typename T>
+vector<Point2D<T>> Polygone<T>::getSommets() const
+{
+    return sommets;
+}
+
+// Accesseurs set
+template <typename T>
+void Polygone<T>::setSommets(const vector<Point2D<T>>& listeSommets)
+{
+    sommets = listeSommets;
+}
+
+// Ajouter un point au polygone
+template <typename T>
+void Polygone<T>::addPoint(const Point2D<T>& point)
+{
+    sommets.push_back(point);
+}
+
+// Translation du polygone
+template <typename T>
+void Polygone<T>::translate(T x, T y)
+{
+    for (auto& point : sommets)
+    {
+        point.translate(x, y);
+    }
+}
+
+// Surcharge de l'opérateur de sortie <<
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const Polygone<T>& poly)
+{
+    os << "Polygone [";
+    const auto& sommets = poly.getSommets();
+    for (size_t i = 0; i < sommets.size(); ++i) {
+        os << sommets[i];
+        if (i < sommets.size() - 1) {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include "Point2D.hpp"
+#include "Polygone.hpp"
 
-int main() {
+using namespace std;
+
+int main()
+{
+    cout << "********************************POINT********************************" << endl;
     // Utilisation de Point2D avec des entiers
     Point2D<int> intPoint1;  // Constructeur par défaut
     Point2D<int> intPoint2(1, 2);  // Constructeur avec valeurs initiales
     Point2D<int> intPoint3(intPoint2);  // Constructeur par recopie
 
     // Affichage des coordonnées des points entiers
-    std::cout << "Point1 - " << intPoint1 << std::endl;
-    std::cout << "Point2 - " << intPoint2 << std::endl;
-    std::cout << "Point3 (copie de Point2) - " << intPoint3 << std::endl;
+    cout << "Point1 - " << intPoint1 << endl;
+    cout << "Point2 - " << intPoint2 << endl;
+    cout << "Point3 (copie de Point2) - " << intPoint3 << endl;
 
     // Translation du point entier
     intPoint2.translate(3, 4);
 
     // Affichage des coordonnées après la translation
-    std::cout << "Après translation - " << intPoint2 << std::endl;
+    cout << "Après translation - " << intPoint2 << endl;
 
     // Utilisation de Point2D avec des doubles
     Point2D<double> doublePoint1;  // Constructeur par défaut
@@ -24,15 +29,15 @@ int main() {
     Point2D<double> doublePoint3(doublePoint2);  // Constructeur par recopie
 
     // Affichage des coordonnées des points doubles
-    std::cout << "Point1 - " << doublePoint1 << std::endl;
-    std::cout << "Point2 - " << doublePoint2 << std::endl;
-    std::cout << "Point3 (copie de Point2) - " << doublePoint3 << std::endl;
+    cout << "Point1 - " << doublePoint1 << endl;
+    cout << "Point2 - " << doublePoint2 << endl;
+    cout << "Point3 (copie de Point2) - " << doublePoint3 << endl;
 
     // Translation du point double
     doublePoint2.translate(1.5, 2.5);
 
     // Affichage des coordonnées après la translation
-    std::cout << "Après translation - " << doublePoint2 << std::endl;
+    cout << "Après translation - " << doublePoint2 << endl;
 
     // Utilisation de Point2D avec des caractères
     Point2D<char> charPoint1;  // Constructeur par défaut
@@ -40,15 +45,73 @@ int main() {
     Point2D<char> charPoint3(charPoint2);  // Constructeur par recopie
 
     // Affichage des coordonnées des points caractères
-    std::cout << "Point1 - " << charPoint1 << std::endl;
-    std::cout << "Point2 - " << charPoint2 << std::endl;
-    std::cout << "Point3 (copie de Point2) - " << charPoint3 << std::endl;
+    cout << "Point1 - " << charPoint1 << endl;
+    cout << "Point2 - " << charPoint2 << endl;
+    cout << "Point3 (copie de Point2) - " << charPoint3 << endl;
 
     // Translation du point caractère
     charPoint2.translate(1, 1);
 
     // Affichage des coordonnées après la translation
-    std::cout << "Après translation - " << charPoint2 << std::endl;
+    cout << "Après translation - " << charPoint2 << endl;
+
+    cout << "********************************POLY********************************" << endl;
+    // Création d'un polygone avec des points entiers
+    Polygone<int> polyInt;
+    polyInt.addPoint(Point2D<int>(1, 2));
+    polyInt.addPoint(Point2D<int>(3, 4));
+    polyInt.addPoint(Point2D<int>(5, 6));
+
+    // Affichage du polygone
+    cout << "Polygone entier : " << polyInt << endl;
+
+    // Translation du polygone
+    polyInt.translate(1, 1);
+
+    // Affichage du polygone après la translation
+    cout << "Polygone entier après translation : " << polyInt << endl;
+
+    // Utilisation du constructeur avec sommets.
+    Polygone<int> constuctorSommets(polyInt.getSommets());
+    cout << "Polygone avec le constructeur des sommets: "  << constuctorSommets << endl;
+
+    // Création d'un polygone avec des points doubles
+    Polygone<double> polyDouble;
+    polyDouble.addPoint(Point2D<double>(2.5, 3.5));
+    polyDouble.addPoint(Point2D<double>(4.5, 5.5));
+
+    // Affichage du polygone
+    cout << "Polygone double : " << polyDouble << endl;
+
+    // Translation du polygone
+    polyDouble.translate(1.5, 2.5);
+
+    // Affichage du polygone après la translation
+    cout << "Polygone double après translation : " << polyDouble << endl;
+
+    // Test de la recopie...
+    cout << "Test de la non-translation d'un polygone post recopie: " << endl;
+    // Création d'un polygone avec des points entiers
+    Polygone<int> originalPoly;
+    originalPoly.addPoint(Point2D<int>(1, 2));
+    originalPoly.addPoint(Point2D<int>(3, 4));
+    originalPoly.addPoint(Point2D<int>(5, 6));
+
+    // Affichage du polygone original
+    cout << "Polygone original : " << originalPoly << endl;
+
+    // Création par recopie d'un nouveau polygone
+    Polygone<int> copiedPoly(originalPoly);
+
+    // Affichage du polygone copié
+    cout << "Polygone copié : " << copiedPoly << endl;
+
+    // Translation du polygone original
+    originalPoly.translate(1, 1);
+
+    // Affichage des polygones après la translation
+    cout << "Polygone original après translation : " << originalPoly << endl;
+    cout << "Polygone copié après translation : " << copiedPoly << endl;
 
     return 0;
 }
